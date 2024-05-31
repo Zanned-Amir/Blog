31bb6270824754f023eb4cc350d57136acd12b10
amir <amirosolaroso34@gmail.com>
2024-05-31 22:52:33 +0100

    add testing for blog_post model

--- test/models/blog_post_test.rb
+++ test/models/blog_post_test.rb
@@ -1,7 +1,34 @@
 require "test_helper"
 
 class BlogPostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def blog_post
+    BlogPost.new(published_at: nil)
+  end
+
+  test "draft? returns true for draft blog post" do
+    assert blog_posts(:draft).draft?
+  end
+
+  test "draft? returns false for published blog post" do
+    refute BlogPost.new(published_at: Time.now).draft?
+  end
+
+  test "published? returns true for scheduled blog post" do
+    refute BlogPost.new(published_at: Time.now + 1.day).published?
+  end
+
+  test "published? returns true for published blog post" do
+    assert BlogPost.new(published_at: 1.year.ago).published?
+  end
+
+  test "published? returns false for draft blog post" do
+    refute blog_post.published?
+  end
+
+  test "published? returns false for scheduled blog post" do
+    refute BlogPost.new(published_at: Time.now + 1.day).published?
+  end
+
+
+
 end
