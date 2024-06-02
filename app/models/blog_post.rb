ce3624a30197afb9b818203bd6c164729eba04ed
amir <amirosolaroso34@gmail.com>
2024-06-02 11:41:42 +0100

    add ActionText for rich text

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,9 +1,10 @@
 class BlogPost < ApplicationRecord
+    has_rich_text :content
     validates :title ,  presence: true
-    validates :body ,  presence: true
+    validates :content ,  presence: true
 
 
-    scope :sorted , -> { order(published_at: :desc , updated_at: :desc) }
+    scope :sorted, -> { order(Arel.sql('COALESCE(published_at, updated_at) DESC')) }
     scope :draft , -> { where(published: nil) }
     scope :published , -> { where("published_at <= ?", Time.now) }
     scope :scheduled , -> { where("published_at > ?", Time.now) }
